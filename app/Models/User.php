1d43e07fa6fc663b21b8ccbd14fd4fa05abbd25b
MohcineSafi <safi.mouhcine02@gmail.com>
2024-05-27 00:49:26 +0100

    Feat:Add models for User, Don and Virement

---
+++ app/Models/User.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use App\Models\Virement;
+use App\Models\Don;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'email',
+        'password',
+        'solde',
+        'is_admin'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+    public function virements(){
+         return $this->hasMany(Virement::class);
+    }
+    public function isAdmin()
+    {
+        return $this->is_admin;
+    }
+    public function dons(){
+        return $this->belongsToMany(Don::class , 'don_user')->withTimestamps();
+    }
+}
